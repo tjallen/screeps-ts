2eacc21d3debd2e1fd39b625bc6b5fedb01b3d50
Thom Allen <thomwork@gmail.com>
2017-06-25 19:48:39 +0100

    Delete unused in config

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,17 +4,3 @@ export const ENABLE_PROFILER: boolean = true;
 export const ENABLE_DEBUG: boolean = true;
 // number of ticks between Utils.debugInfo() calls
 export const DEBUG_THROTTLE: number = 20;
-// generic worker count
-// TODO move to worker.ts
-// TODO edit count depending on sources & available tiles
-export const CREEP_COUNT_WORKER: object = {
-  RCL: {
-    1: 4,
-    2: 3,
-    3: 2,
-    4: 1,
-  }
-};
-// generic worker body
-// TODO multiply body size depending on RCL / energy avail
-export const BODY_WORKER: string[] = [WORK, CARRY, MOVE];
