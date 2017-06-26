c8a9e239f85da6b3cb4074d787faddb816557c80
Thom Allen <thomwork@gmail.com>
2017-06-26 15:32:32 +0100

    Clear / garbage collect dead creeps from memory

--- src/components/creepManager.ts
+++ src/components/creepManager.ts
@@ -3,10 +3,19 @@ import * as Config from './../config';
 import * as Utils from './../utils';
 import Roles from './../roles';
 
+function garbageCollection(creeps): void {
+  // clear dead creeps from Memory
+  for (let index in Memory.creeps) {
+    if (!creeps[index]) {
+      delete Memory.creeps[index];
+    }
+  }
+}
+
 export function run(creeps : object): void {
+  if (Game.time % Config.GARBAGE_COLLECTION_THROTTLE === 0) garbageCollection(creeps);
   // fire role.run() for each creep
-  _.forEach(creeps, (creep: Creep) => {
-    // console.log('=>', creep)
+  _.forEach(creeps, (creep: Creep, index) => {
     // creep.suicide()
     if ((!creep.memory.role) || (!Roles.hasOwnProperty(creep.memory.role))) {
       console.log(`[ERR] attempting role.run(): No role in memory or unknown role [${creep.name} in ${creep.pos.roomName}]`)
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,8 @@
 // true enables screeps-profiler
-export const ENABLE_PROFILER: boolean = true;
+export const ENABLE_PROFILER: boolean = false;
 // true enables debug msgs
 export const ENABLE_DEBUG: boolean = true;
 // number of ticks between Utils.debugInfo() calls
 export const DEBUG_THROTTLE: number = 20;
+// ticks between Memory GC
+export const GARBAGE_COLLECTION_THROTTLE: number = 30;
